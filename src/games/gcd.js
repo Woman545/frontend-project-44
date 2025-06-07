95277761369d19a1ebda57d78279efe701e90f26
Woman545 <doroshevich.2019@bk.ru>
2025-06-08 00:45:45 +0300

    irgaHOD

---
+++ src/games/gcd.js
@@ -0,0 +1,21 @@
+import getRandomNumber from '../utils/random.js';
+import runGame from '../index.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (a, b) => {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(getGcd(num1, num2));
+  return [question, correctAnswer];
+};
+
+export default () => runGame(description, generateRound);
