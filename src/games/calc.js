35b61de44c85efd73ee7274ce27ead619139f10d
Woman545 <doroshevich.2019@bk.ru>
2025-06-08 00:24:43 +0300

    calkulator

---
+++ src/games/calc.js
@@ -0,0 +1,31 @@
+import getRandomNumber from '../utils/random.js';
+import runGameEngine from '../index.js';
+
+const description = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unknown operator: ${operator}`);
+  }
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 30);
+  const num2 = getRandomNumber(1, 30);
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = String(calculate(num1, num2, operator));
+
+  return [question, correctAnswer];
+};
+
+export default () => runGameEngine(description, generateRound);
