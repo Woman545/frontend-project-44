dbfcbadf8d28d8d6a1b90513dadf3d134c1241b4
Woman545 <doroshevich.2019@bk.ru>
2025-06-09 15:50:16 +0300

    Fixes.eslint2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,17 @@ module.exports = {
     es2022: true,
     node: true,
   },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  extends: ['eslint:recommended'],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    semi: ['error', 'never'],
     'no-console': 'off',
+    'import/extensions': 'off',
+    '@stylistic/semi': 'off',
+    '@stylistic/quotes': 'off',
+    '@stylistic/max-statements-per-line': 'off',
+    '@stylistic/brace-style': 'off',
+    '@stylistic/quote-props': 'off',
+    '@stylistic/comma-dangle': 'off',
+    '@stylistic/eol-last': 'off',
   },
-}
+};
