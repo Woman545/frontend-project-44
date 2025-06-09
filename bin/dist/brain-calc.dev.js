fa4f2374cf3757a985bbfab96d252d7fd236194e
Woman545 <doroshevich.2019@bk.ru>
2025-06-09 20:25:29 +0300

    Fixes4

--- bin/dist/brain-calc.dev.js
+++ bin/dist/brain-calc.dev.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-"use strict"
+'use strict'
 
-var _calc = _interopRequireDefault(require("../src/games/calc.js"))
+var _calc = _interopRequireDefault(require('../src/games/calc.js'))
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj } }
 
-(0, _calc["default"])()
+(0, _calc['default'])()
