9a02a03abd26112ba036b2522e5009c675696348
Woman545 <doroshevich.2019@bk.ru>
2025-06-09 15:30:23 +0300

    code12

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,9 @@ module.exports = {
     es2022: true,
     node: true,
   },
+  plugins: ['prettier'],
   rules: {
     'no-console': 'off',
     'import/extensions': 'off',
   },
-  plugins: ['prettier'],
 };
